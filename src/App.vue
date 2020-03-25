6ad048662e2ed091db7cf70e8a54c6c8dd82b3ca
Zakaria Santoso <zgamingxxvi@gmail.com>
2020-03-25 23:10:53 +0700

    Added Base Project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <layout>
+      <template v-slot:navigation>
+        <main-navigation />
+      </template>
+      <router-view />
+    </layout>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Layout from "./layouts/default.vue"
+import MainNavigation from "./components/elements/MainNavigation"
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    Layout,
+    MainNavigation
   }
 }
+</script>
+
+<style lang="scss">
 </style>
