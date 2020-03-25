1c88edb83083a272554d21f9665c0887eba9e326
Zakaria Santoso <zgamingxxvi@gmail.com>
2020-03-25 23:50:02 +0700

    Added App Button

---
+++ src/components/frames/inputs/AppButton.vue
@@ -0,0 +1,53 @@
+<template>
+	<button :class="{'full-width': isFullWidth}"
+		@click="$emit('click')"
+		:disabled="isLoading"
+	>
+		<app-loading :size="15" v-if="isLoading" color="white"/>
+		<slot v-else/>
+	</button>
+</template>
+
+<script>
+import AppLoading from '../AppLoading'
+
+export default {
+	components: {
+		AppLoading
+	},
+
+	props: {
+		isFullWidth: {
+			type: Boolean,
+			default: false
+		},
+		isLoading: {
+			type: Boolean,
+			default: false
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+button {
+	padding: 5px 10px;
+	min-width: 120px;
+
+	background-color: $main-background-color;
+	border: 1px solid gray;
+	border-radius: 5px;
+	color: $main-text-color;
+	// line-height: 16px;
+	font-size: 16px;
+	outline: none;
+
+	&:hover {
+		font-weight: bold;
+	}	
+
+	&:active {
+		background-color: rgba($color: black, $alpha: 0.5);
+	}
+}
+</style>
