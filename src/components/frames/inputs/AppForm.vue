f3168500f22c5ac89e44dbd06a5afd918f2e22a2
Zakaria Santoso <zgamingxxvi@gmail.com>
2020-03-26 00:53:39 +0700

    Added App Form

---
+++ src/components/frames/inputs/AppForm.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="app-form">
+		<h3 v-if="title">{{ title }}</h3>
+		<form @submit.prevent>
+			<slot />
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: false
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+.app-form {
+	display: flex;
+	flex-direction: column;
+	padding: 0px 30px 30px;
+
+	border: 1px solid black;
+	border-radius: 5px;
+
+	h3 {
+		text-align: center;
+	}
+}
+</style>
