36c0e5227f207422e4ed244dde3f9a51bebee4a4
Zakaria Santoso <zgamingxxvi@gmail.com>
2020-03-25 23:09:27 +0700

    Added Main Navigation

---
+++ src/components/elements/MainNavigation.vue
@@ -0,0 +1,63 @@
+<template>
+	<nav>
+		<ul>
+			<li>
+				<router-link to="/">Home</router-link>
+			</li>
+			<li>
+				<router-link to="/todo">Todo List</router-link>
+			</li>
+		</ul>
+
+		<ul>
+			<li>
+				<router-link to="/note">Notes</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+nav {
+	display: flex;
+	justify-content: space-between;
+	
+	ul {
+		display: flex;
+		margin: 0px;
+		padding: 0px;
+
+		list-style: none;
+	}
+
+	li {
+		display: block;
+	}
+
+	a {
+		display: block;
+		padding: 10px;
+		min-width: 120px;
+
+		color: $main-text-color;
+		text-decoration: none;
+		text-align: center;
+
+		&:hover {
+			background-color: $main-hover-background-color;
+			color: black;
+		}
+
+		&.router-link-exact-active {
+			text-decoration: underline;
+			font-weight: bold;
+		}
+	}
+}
+</style>
