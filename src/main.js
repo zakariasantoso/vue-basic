51005a3e2ff639e7f4c9233aff6c227639c51602
Zakaria Santoso <zgamingxxvi@gmail.com>
2020-03-28 20:27:06 +0700

    Added Global Mixin

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mixin from './mixins/'
 
 import './assets/css/style.scss'
 
 Vue.config.productionTip = false
-
+Vue.mixin(mixin)
 new Vue({
   router,
   store,
